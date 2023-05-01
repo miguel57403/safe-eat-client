781f64a2b020a393b9353d60bd6cf9e3a2515802
daniel57060 <a57060@alunos.ipb.pt>
2023-05-01 01:31:26 +0100

    Create register_activity

--- app/src/main/java/mb/safeEat/LoginActivity.kt
+++ app/src/main/java/mb/safeEat/LoginActivity.kt
@@ -1,12 +1,25 @@
 package mb.safeEat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar?.hide()
+
+        val textViewWithoutAccount = findViewById<TextView>(R.id.textViewWithoutAccount)
+        val textViewSignUp = findViewById<TextView>(R.id.textViewSignUp)
+
+        textViewWithoutAccount.setOnClickListener { navigateToRegister() }
+        textViewSignUp.setOnClickListener { navigateToRegister() }
+    }
+
+    private fun navigateToRegister() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
