56682eebba200109997b37b20979ad313a5f83a1
daniel57060 <a57060@alunos.ipb.pt>
2023-05-12 20:29:52 +0100

    Experiment bottom navigation

--- app/src/main/java/mb/safeEat/MainActivity.kt
+++ app/src/main/java/mb/safeEat/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // https://www.youtube.com/watch?v=Bb8SgfI4Cm4&ab_channel=Foxandroid
     }
 }
