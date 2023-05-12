3fc501b6de6ce1dd397481ce05d464cc7d13ef9a
daniel57060 <a57060@alunos.ipb.pt>
2023-05-12 18:45:04 +0100

    Create all activities

---
+++ app/src/main/java/mb/safeEat/AddressRegisterActivity.kt
@@ -0,0 +1,11 @@
+package mb.safeEat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddressRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_address_register)
+    }
+}
