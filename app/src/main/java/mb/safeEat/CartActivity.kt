52066b2be7f69c386c51e9a92add4d88a2160093
daniel57060 <a57060@alunos.ipb.pt>
2023-05-23 17:05:17 +0100

    Refactor with ConstraintLayout

--- app/src/main/java/mb/safeEat/CartActivity.kt
+++ app/src/main/java/mb/safeEat/CartActivity.kt
@@ -1,12 +1,12 @@
 package mb.safeEat
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
@@ -20,59 +20,65 @@ class CartActivity : AppCompatActivity() {
         setContentView(R.layout.activity_cart)
         initAdapter()
     }
+
     private fun initAdapter() {
         val adapter = CartAdapter(createList())
-        val rv_home = findViewById<RecyclerView>(R.id.rv_home)
-        rv_home.layoutManager = LinearLayoutManager(this)
-        rv_home.adapter = adapter
+        val items = findViewById<RecyclerView>(R.id.cart_items)
+        items.layoutManager = LinearLayoutManager(this)
+        items.adapter = adapter
     }
+
     private fun createList(): ArrayList<Product> {
         val list = ArrayList<Product>()
-        list.add(Product(product = "Pizza acebolada", quantifyItem = 3, priceItem = 14.99F))
-        list.add(Product(product = "Pizza 4 queijos", quantifyItem = 1, priceItem = 2.99F))
-        list.add(Product(product = "Pizza de achova", quantifyItem = 2, priceItem = 2.99F))
+        list.add(Product(product = "Pizza acebolada", quantifyItem = 3, priceItem = 14.99F, warn = true))
+        list.add(Product(product = "Pizza 4 queijos", quantifyItem = 1, priceItem = 2.99F, warn = false))
+        list.add(Product(product = "Pizza de achova", quantifyItem = 2, priceItem = 2.99F, warn = false))
         return list
     }
 }
 
-class CartAdapter(private var data: ArrayList<Product>) : RecyclerView.Adapter<CartAdapter.ViewHolder>() {
+class CartAdapter(private var data: ArrayList<Product>) :
+    RecyclerView.Adapter<CartAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.cart_item, parent, false))
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.cart_item, parent, false)
+        )
     }
+
     override fun getItemCount(): Int {
         return data.size
     }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.bindView(item, position)
+        holder.bindView(item)
     }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val containermeu = itemView.findViewById<LinearLayout>(R.id.containerMeu)
-        val product = itemView.findViewById<TextView>(R.id.cart_item_product)
-        val quantifyItem = itemView.findViewById<TextView>(R.id.quantifyItem)
-        val priceItem = itemView.findViewById<TextView>(R.id.priceItem)
-        val icon = itemView.findViewById<ImageView>(R.id.iconWarning)
-        fun bindView(item: Product,position: Int) {
-            if(position == 1 ){
-                val bg = ContextCompat.getDrawable(itemView.context, R.drawable.item_without_border_)
-                containermeu.background = bg
-                icon.visibility = View.GONE
-            }
-            if(position == 2 ){
-                val bg = ContextCompat.getDrawable(itemView.context, R.drawable.item_without_border_)
-                containermeu.background = bg
-                icon.visibility = View.GONE
+        private val container = itemView.findViewById<ConstraintLayout>(R.id.cart_item_container)
+        private val product = itemView.findViewById<TextView>(R.id.cart_item_name)
+        private val quantity = itemView.findViewById<TextView>(R.id.cart_item_quantity)
+        private val price = itemView.findViewById<TextView>(R.id.cart_item_price)
+        private val icon = itemView.findViewById<ImageView>(R.id.cart_item_alert)
+
+        @SuppressLint("SetTextI18n")
+        fun bindView(item: Product) {
+            if (item.warn) {
+                val bg =
+                    ContextCompat.getDrawable(itemView.context, R.drawable.border_item_warning)
+                container.background = bg
+                icon.visibility = View.VISIBLE
             }
             product.text = item.product
-            quantifyItem.text = item.quantifyItem.toString()
-            priceItem.text = ("€" + item.priceItem.toString())
-
+            quantity.text = item.quantifyItem.toString()
+            price.text = ("€" + item.priceItem.toString())
         }
     }
-
 }
+
 data class Product(
     val product: String,
     val quantifyItem: Int,
     val priceItem: Float,
+    val warn: Boolean
 )
