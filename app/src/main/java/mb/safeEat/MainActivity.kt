8a0fcf999b5c939174768a4e0610b738d1384fb0
daniel57060 <a57060@alunos.ipb.pt>
2023-05-23 20:55:56 +0100

    Change screen on click on bottom navigation

--- app/src/main/java/mb/safeEat/MainActivity.kt
+++ app/src/main/java/mb/safeEat/MainActivity.kt
@@ -2,11 +2,39 @@ package mb.safeEat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.lang.Exception
+
+// Bottom Navigation: https://www.youtube.com/watch?v=Bb8SgfI4Cm4&ab_channel=Foxandroid
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // https://www.youtube.com/watch?v=Bb8SgfI4Cm4&ab_channel=Foxandroid
+        if (savedInstanceState == null) onInit()
+    }
+
+    private fun onInit() {
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigation.setOnItemSelectedListener {
+            Log.d("Click", it.itemId.toString())
+            val fragment = when (it.itemId) {
+                R.id.menu_item_home -> HomeFragment()
+                R.id.menu_item_search -> SearchFragment()
+                R.id.menu_item_cart -> CartFragment()
+                R.id.menu_item_user -> UserFragment()
+                R.id.menu_item_notification -> NotificationsFragment()
+                else -> throw Exception("Menu item id not defined")
+            }
+
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_body, fragment)
+                .commit()
+
+            true
+        }
     }
 }
