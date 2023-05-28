966500b47f9f2b9cb72bce387b876875e6552889
daniel57060 <a57060@alunos.ipb.pt>
2023-05-29 00:06:02 +0100

    Refactor to merge

--- app/src/main/java/mb/safeEat/PaymentOptionActivity.kt
+++ app/src/main/java/mb/safeEat/PaymentOptionActivity.kt
@@ -2,10 +2,20 @@ package mb.safeEat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.google.android.material.card.MaterialCardView
 
 class PaymentOptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment_option)
+        initHeader()
+    }
+
+    private fun initHeader() {
+        val title = findViewById<TextView>(R.id.header_title)
+        val backButton = findViewById<MaterialCardView>(R.id.header_back_button)
+        title.text = resources.getString(R.string.t_payment_options)
+        backButton.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
 }
