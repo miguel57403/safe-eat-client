dbea0a45ba61dcd68d7d3e17386bbef2b6ebfa99
Miguel Beckers <128187386+miguel57403@users.noreply.github.com>
2023-07-01 17:16:57 +0100

    integration AddressFragment.kt

--- app/src/main/java/mb/safeEat/components/AddressFragment.kt
+++ app/src/main/java/mb/safeEat/components/AddressFragment.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.android.material.card.MaterialCardView
 import mb.safeEat.R
+import mb.safeEat.functions.suspendToLiveData
+import mb.safeEat.services.api.api
 
 class AddressFragment(private val navigation: NavigationListener) : Fragment() {
     override fun onCreateView(
@@ -21,11 +23,22 @@ class AddressFragment(private val navigation: NavigationListener) : Fragment() {
 
     private fun onInit(view: View) {
         initHeader(view)
+
+        val addressName = view.findViewById<TextView>(R.id.address_main_text)
+        suspendToLiveData { api.addresses.findAll() }.observe(viewLifecycleOwner) { result ->
+            result.fold(onSuccess = { addresses ->
+                // TODO: Add a RecyclerView to show all addresses
+                addressName.text = addresses[0].name
+            }, onFailure = {
+                println("Api Error")
+            })
+        }
     }
 
     private fun initHeader(view: View) {
         val title = view.findViewById<TextView>(R.id.header_title)
         val backButton = view.findViewById<MaterialCardView>(R.id.header_back_button)
+
         title.text = resources.getString(R.string.t_address)
         backButton.setOnClickListener { navigation.onBackPressed() }
     }
