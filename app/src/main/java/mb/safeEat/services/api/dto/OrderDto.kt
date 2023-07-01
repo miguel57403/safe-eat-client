7981193cfe4b99530453b8a4bd5c541204417b49
Alexandr33059 <a33059@alunos.ipb.pt>
2023-07-01 19:11:41 +0100

    Integration RestricitionAlertDialog..need to complete

--- app/src/main/java/mb/safeEat/services/api/dto/OrderDto.kt
+++ app/src/main/java/mb/safeEat/services/api/dto/OrderDto.kt
@@ -1,9 +1,7 @@
 package mb.safeEat.services.api.dto
 
 data class OrderDto(
-    val id: String? = null,
     val addressId: String? = null,
     val paymentId: String? = null,
     val deliveryId: String? = null,
-    val itemIds: List<String>? = null
 )
