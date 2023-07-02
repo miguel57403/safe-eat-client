bf063f06c497f7caabfaec102870f83dcc2a4cf7
daniel57060 <a57060@alunos.ipb.pt>
2023-07-02 09:12:56 +0100

    Delete api.model.Login.kt

--- app/src/main/java/mb/safeEat/services/api/models/Login.kt
+++
@@ -1,9 +0,0 @@
-package mb.safeEat.services.api.models
-
-data class Login(
-    val status: String? = null,
-    val message: String? = null,
-    val token: String? = null,
-)
-
-// TODO: not used anymore
