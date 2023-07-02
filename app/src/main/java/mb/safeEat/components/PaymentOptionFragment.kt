00d3f0270b0c1ca4456dafb171da4588b859b670
Eduardo <dukrl222@hotmail.com>
2023-07-02 16:25:25 +0100

    ISSUE 91 - Corrigida tela de listagem dos pagamentos

--- app/src/main/java/mb/safeEat/components/PaymentOptionFragment.kt
+++ app/src/main/java/mb/safeEat/components/PaymentOptionFragment.kt
@@ -1,16 +1,27 @@
 package mb.safeEat.components
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
 import mb.safeEat.R
 import mb.safeEat.extensions.Alertable
+import mb.safeEat.functions.suspendToLiveData
+import mb.safeEat.services.api.api
 
-class PaymentOptionFragment(private val navigation: NavigationListener) : Fragment(), Alertable {
+class PaymentOptionFragment(private val navigation: NavigationListener) : Fragment(), PaymentListener, Alertable {
+
+    private lateinit var items: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,12 +30,103 @@ class PaymentOptionFragment(private val navigation: NavigationListener) : Fragme
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initHeader(view)
+        initAdapter(view)
+        initScreenEvents(view)
+        loadInitialData()
     }
 
     private fun initHeader(view: View) {
         val title = view.findViewById<TextView>(R.id.header_title)
         val backButton = view.findViewById<MaterialCardView>(R.id.header_back_button)
-        title.text = resources.getString(R.string.t_payment_options)
+
+        title.text = resources.getString(R.string.t_payment)
         backButton.setOnClickListener { navigation.onBackPressed() }
     }
+
+    private fun initAdapter(view: View) {
+        items = view.findViewById(R.id.payment_select_items)
+        items.layoutManager = LinearLayoutManager(view.context)
+        items.adapter = PaymentAdapter(this)
+    }
+
+    private fun initScreenEvents(view: View) {
+        val button = view.findViewById<Button>(R.id.payment_select_submit)
+        button.setOnClickListener {
+            navigation.navigateTo(PaymentFragment(navigation))
+        }
+    }
+
+    private fun loadInitialData() {
+        suspendToLiveData { api.payments.findAll() }.observe(viewLifecycleOwner) { result ->
+            result.fold(onSuccess = { payments ->
+                val initialData = mapInitialData(payments)
+                (items.adapter as PaymentAdapter).loadInitialData(initialData)
+            }, onFailure = {
+                alertThrowable(it)
+            })
+        }
+    }
+
+    private fun mapInitialData(payment: List<mb.safeEat.services.api.models.Payment>): ArrayList<Payment> {
+        return payment.map { Payment(it.name!!, false) }.toCollection(ArrayList())
+    }
+
+    override fun onPaymentSelected(payment: Payment) {
+        // TODO: Implement this
+        Log.d("Click", "Payment Clicked Selected")
+    }
+
+    override fun onPaymentEdit(payment: Payment) {
+        // TODO: Implement this
+        Log.d("Click", "Payment Clicked Edit")
+    }
 }
+
+interface PaymentListener {
+    fun onPaymentSelected(payment: Payment)
+    fun onPaymentEdit(payment: Payment)
+}
+
+class PaymentAdapter(private val listener: PaymentListener) :
+    RecyclerView.Adapter<PaymentAdapter.ViewHolder>() {
+    private var data = ArrayList<Payment>()
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun loadInitialData(newData: ArrayList<Payment>) {
+        data = newData
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        listener,
+        LayoutInflater.from(parent.context).inflate(R.layout.item_payment, parent, false)
+    )
+
+    override fun getItemCount(): Int = data.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position])
+
+    class ViewHolder(private val listener: PaymentListener, itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        private val container = itemView.findViewById<MaterialCardView>(R.id.item_payment_container)
+        private val text = itemView.findViewById<TextView>(R.id.item_payment_text)
+        private val icon = itemView.findViewById<ImageView>(R.id.item_payment_icon1)
+        private val editIcon = itemView.findViewById<MaterialCardView>(R.id.item_payment_icon2)
+
+        fun bind(item: Payment) {
+            text.text = item.text
+            if (item.default) {
+                icon.setImageResource(R.drawable.baseline_home_24)
+            } else {
+                icon.setImageResource(R.drawable.baseline_business_24)
+            }
+            container.setOnClickListener { listener.onPaymentSelected(item) }
+            editIcon.setOnClickListener { listener.onPaymentEdit(item) }
+        }
+    }
+}
+
+data class Payment(
+    val text: String,
+    val default: Boolean
+)
