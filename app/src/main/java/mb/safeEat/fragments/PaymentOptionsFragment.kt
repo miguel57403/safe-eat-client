b290733567f06ef3dc1e4963b195a7a601ec5351
daniel57060 <a57060@alunos.ipb.pt>
2023-07-06 13:06:08 +0100

    Refactor to merge

--- app/src/main/java/mb/safeEat/fragments/PaymentOptionsFragment.kt
+++ app/src/main/java/mb/safeEat/fragments/PaymentOptionsFragment.kt
@@ -49,7 +49,7 @@ class PaymentOptionsFragment(private val navigation: NavigationListener) : Fragm
     private fun initScreenEvents(view: View) {
         val button = view.findViewById<Button>(R.id.payment_select_submit)
         button.setOnClickListener {
-            navigation.navigateTo(PaymentDetailFragment(navigation))
+            navigation.navigateTo(PaymentRegisterFragment(navigation))
         }
     }
 
--- app/src/main/java/mb/safeEat/fragments/PaymentRegisterFragment.kt
+++ app/src/main/java/mb/safeEat/fragments/PaymentRegisterFragment.kt
@@ -1,49 +1,28 @@
-package mb.safeEat.components
+package mb.safeEat.fragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
-import androidx.core.content.ContextCompat
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
 import mb.safeEat.R
+import mb.safeEat.activities.NavigationListener
 import mb.safeEat.extensions.Alertable
-import mb.safeEat.functions.suspendToLiveData
-import mb.safeEat.services.api.api
-import mb.safeEat.services.api.models.Ingredient
 
-class PaymentDetailFragment(private val navigation: NavigationListener) : Fragment(),
-    PaymentListener, Alertable {
-    override fun onPaymentSelected(payment: Payment) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onPaymentEdit(payment: Payment) {
-        TODO("Not yet implemented")
-    }
-
+class PaymentRegisterFragment(private val navigation: NavigationListener) : Fragment(), Alertable {
+    // TODO: Finish implementation
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_payment_detail, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_payment_register, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initHeader(view)
         iniSpinner(view)
-        //initAdapter(view)
         //initScreenEvents(view)
         //loadInitialData()
     }
@@ -61,7 +40,7 @@ class PaymentDetailFragment(private val navigation: NavigationListener) : Fragme
         val type = arrayOf("Credit", "Debit")
 
         spinnerId.adapter =
-            this.context?.let { ArrayAdapter(it, android.R.layout.simple_spinner_dropdown_item, type) }
+            ArrayAdapter(view.context, android.R.layout.simple_spinner_dropdown_item, type)
     }
 
 }
